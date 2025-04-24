256b07685c03080e9473e956b17269ca243359d7
SikanderRasheed <sikanderb069@gmail.com>
2025-04-24 10:26:08 -0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,79 @@
+// Counter Code Start
+function animateCounter(element, start, end, duration) {
+    let startTime = null;
+
+    function update(currentTime) {
+        if (!startTime) startTime = currentTime;
+        const progress = Math.min((currentTime - startTime) / duration, 1);
+        const value = Math.floor(progress * (end - start) + start);
+        element.textContent = value.toLocaleString();
+
+        if (progress < 1) {
+            requestAnimationFrame(update);
+        }
+    }
+
+    requestAnimationFrame(update);
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    const counters = document.querySelectorAll('.counter-number');
+
+    const observerOptions = {
+        root: null,
+        threshold: 0.5,
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const counter = entry.target;
+                const endValue = parseInt(counter.getAttribute('data-target'));
+
+                if (!counter.classList.contains('animated')) {
+                    animateCounter(counter, 0, endValue, 2000);
+                    counter.classList.add('animated');
+                }
+
+                observer.unobserve(counter);
+            }
+        });
+    }, observerOptions);
+
+    counters.forEach(counter => {
+        observer.observe(counter);
+    });
+});
+
+// Counter Code End
+
+// Why Choose Slider Start
+
+
+$('.choose_slider').slick({
+    infinite: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+
+// Why Choose Slider End
+
+// ServicesSlider Start
+$('.service_slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 7000,
+    cssEase: 'linear',
+    infinite: true,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    pauseOnFocus: false
+});
+
+//   ServicesSlider End
